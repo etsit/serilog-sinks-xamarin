79cb403b0f0b3961a667d305e29a293dec9d8d37
Daniel Wetterbro <daniel@wetterbro.se>
2015-10-21 17:35:00 +0200

    File rolling
    Functionality from Serilog.FullNetFx inserted.

--- src/Serilog.Sinks.MonoAndroid/LoggerConfigurationMonoAndroidExtensions.cs
+++ src/Serilog.Sinks.MonoAndroid/LoggerConfigurationMonoAndroidExtensions.cs
@@ -17,6 +17,7 @@ using Serilog.Configuration;
 using Serilog.Events;
 using Serilog.Formatting.Display;
 using Serilog.Sinks.MonoAndroid;
+using Serilog.Sinks.RollingFile;
 
 namespace Serilog
 {
@@ -26,6 +27,10 @@ namespace Serilog
 	public static class LoggerConfigurationMonoAndroidExtensions
 	{
 		const string DefaultAndroidLogOutputTemplate = "[{Level}] {Message:l{NewLine:l}{Exception:l}";
+        const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
+        const string DefaultConsoleOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}";
+        const long DefaultFileSizeLimitBytes = 1L * 1024 * 1024 * 1024;
+        const int DefaultRetainedFileCountLimit = 31; // A long month of logs
 
 	    /// <summary>
 	    /// Write to the built-in Android log.
@@ -51,5 +56,41 @@ namespace Serilog
 			var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
 			return sinkConfiguration.Sink(new AndroidLogSink(formatter), restrictedToMinimumLevel);
 		}
+
+        /// <summary>
+        /// Write log events to a series of files. Each file will be named according to
+        /// the date of the first log entry written to it. Only simple date-based rolling is
+        /// currently supported.
+        /// </summary>
+        /// <param name="sinkConfiguration">Logger sink configuration.</param>
+        /// <param name="pathFormat">String describing the location of the log files,
+        /// with {Date} in the place of the file date. E.g. "Logs\myapp-{Date}.log" will result in log
+        /// files such as "Logs\myapp-2013-10-20.log", "Logs\myapp-2013-10-21.log" and so on.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for
+        /// events passed through the sink.</param>
+        /// <param name="outputTemplate">A message template describing the format used to write to the sink.
+        /// the default is "{Timestamp} [{Level}] {Message}{NewLine}{Exception}".</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <param name="fileSizeLimitBytes">The maximum size, in bytes, to which any single log file will be allowed to grow.
+        /// For unrestricted growth, pass null. The default is 1 GB.</param>
+        /// <param name="retainedFileCountLimit">The maximum number of log files that will be retained,
+        /// including the current log file. For unlimited retention, pass null. The default is 31.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        /// <remarks>The file will be written using the UTF-8 character set.</remarks>
+        public static LoggerConfiguration RollingFile(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string pathFormat,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            string outputTemplate = DefaultOutputTemplate,
+            IFormatProvider formatProvider = null,
+            long? fileSizeLimitBytes = DefaultFileSizeLimitBytes,
+            int? retainedFileCountLimit = DefaultRetainedFileCountLimit)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException("sinkConfiguration");
+            if (outputTemplate == null) throw new ArgumentNullException("outputTemplate");
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+            var sink = new RollingFileSink(pathFormat, formatter, fileSizeLimitBytes, retainedFileCountLimit);
+            return sinkConfiguration.Sink(sink, restrictedToMinimumLevel);
+        }
 	}
 }
